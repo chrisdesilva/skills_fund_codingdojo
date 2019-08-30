d078028760e156902e434a4dd4b0ed7f82c62f4a
Chris DeSilva <chris.a.desilva@gmail.com>
2019-08-30 15:38:49 -0500

    Add Yotpo widget

--- src/components/applyFooter.js
+++ src/components/applyFooter.js
@@ -16,6 +16,16 @@ const ApplyFooter = props => {
                     /> 
                 </div>
             </div>
+            <div class="yotpo yotpo-reviews-carousel" 
+                data-background-color="transparent" 
+                data-mode="top_rated" 
+                data-type="site" 
+                data-count="9" 
+                data-show-bottomline="1" 
+                data-autoplay-enabled="1" 
+                data-autoplay-speed="3000" 
+                data-show-navigation="1">&nbsp;
+            </div>
         </div>
     )
 }
