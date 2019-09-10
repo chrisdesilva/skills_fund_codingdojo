56474cc240808a0e9ca1e3e3117dcc1828e42bc4
Chris DeSilva <chris.a.desilva@gmail.com>
2019-09-10 17:26:44 -0500

    Update GA ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: [
-          "UA-99765669-6"
+          "UA-68312423-1"
         ]
       }
     },
