d4f63961fd33f58562cf421b56421fc21072b7cb
Chris DeSilva <chris.a.desilva@gmail.com>
2019-09-19 14:43:36 -0500

    Add Yotpo reviews carousel

---
+++ src/components/reviews.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Reviews = () => (
+    <div class="yotpo yotpo-reviews-carousel"
+ data-background-color="transparent"
+ data-mode="top_rated"
+ data-type="site"
+ data-count="9"
+ data-show-bottomline="1"
+ data-autoplay-enabled="1"
+ data-autoplay-speed="3000"
+ data-show-navigation="1">&nbsp;</div>
+)
