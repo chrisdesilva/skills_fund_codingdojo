62bdfd5c910640559c745ea6471352c3b302ead6
Chris DeSilva <chris@skills.fund>
2020-04-22 14:37:12 -0500

    Add part-time flex program

--- src/constants/programInfo.js
+++ src/constants/programInfo.js
@@ -96,6 +96,12 @@ export const faq = {
       col: true,
       colAmount: "$4,500",
     },
+    {
+      programName: "Coding Dojo's Online Part-time Flex Bootcamp",
+      maxAmount: "$8,995",
+      col: false,
+      colAmount: "$4,500",
+    },
   ],
 }
 
@@ -482,6 +488,84 @@ export const programLoanInfo = [
             k: 6,
             apr36: 11.5,
             apr60: 12.84,
+          },
+          "Immediate Repayment": {
+            apr36: 11.25,
+            apr60: 12.55,
+          },
+        },
+      },
+      {
+        location: "Metro B",
+        loanInfo: {
+          maxLoanAmt: 15545,
+          loanTerm36: true,
+          loanTerm60: true,
+          "Interest Only": {
+            k: 6,
+            apr36: 11.59,
+            apr60: 12.84,
+          },
+          "Immediate Repayment": {
+            apr36: 11.25,
+            apr60: 12.55,
+          },
+        },
+      },
+      {
+        location: "Metro C",
+        loanInfo: {
+          maxLoanAmt: 20545,
+          loanTerm36: true,
+          loanTerm60: true,
+          "Interest Only": {
+            k: 6,
+            apr36: 11.59,
+            apr60: 12.84,
+          },
+          "Immediate Repayment": {
+            apr36: 11.25,
+            apr60: 12.55,
+          },
+        },
+      },
+    ],
+  },
+  {
+    name: "Online Part-time Flex Bootcamp",
+    url: "https://my.skills.fund/application?lenderCode=SFCDOPTFB20",
+    loanInfo: {
+      // match loanInfo in first metro below
+      maxLoanAmt: 8995,
+      loanTerm36: true,
+      loanTerm60: true,
+      "Interest Only": {
+        k: 10,
+        apr36: 11.26,
+        apr60: 12.69,
+      },
+      "Immediate Repayment": {
+        apr36: 11.25,
+        apr60: 12.55,
+      },
+    },
+    defaultLoanType: "Interest Only", // leave at 0 for interest-only, set to 1 for immediate repayment
+    showMetros: false,
+    showLoanTypes: false,
+    loanTypes: ["Interest Only"],
+    locations: ["Metro A", "Metro B", "Metro C"],
+    metros: [
+      {
+        location: "Metro A",
+        loanInfo: {
+          // match loanInfo to Program 3 loanInfo above
+          maxLoanAmt: 8995,
+          loanTerm36: true,
+          loanTerm60: true,
+          "Interest Only": {
+            k: 10,
+            apr36: 11.26,
+            apr60: 12.69,
           },
           "Immediate Repayment": {
             apr36: 11.25,
