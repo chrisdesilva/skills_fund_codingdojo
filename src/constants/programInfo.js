a512a887051fa0a5cd97c8dad8c2cf05806537ff
Arthur Kang <akang@goalsolutions.com>
2021-02-05 22:34:35 -0800

    Remove Online Part-Time Bootcamp

--- src/constants/programInfo.js
+++ src/constants/programInfo.js
@@ -143,12 +143,12 @@ export const faq = {
       col: false,
       colAmount: "$6,000",
     },
-    {
-      programName: "Coding Dojo's Online Part-Time Bootcamp",
-      maxAmount: "$7,995",
-      col: false,
-      colAmount: "$6,000",
-    },
+    // {
+    //   programName: "Coding Dojo's Online Part-Time Bootcamp",
+    //   maxAmount: "$7,995",
+    //   col: false,
+    //   colAmount: "$6,000",
+    // },
     {
       programName: "Coding Dojo's Part-Time Online Data Science program",
       maxAmount: "$4,995",
@@ -442,84 +442,84 @@ export const programLoanInfo = [
       },
     ],
   },
-  {
-    name: "Online Part-Time Bootcamp",
-    url: "https://my.skills.fund/application?lenderCode=SKDOJON18",
-    loanInfo: {
-      // match loanInfo in first metro below
-      maxLoanAmt: 6995,
-      loanTerm36: true,
-      loanTerm60: true,
-      "Interest Only": {
-        k: 6,
-        apr36: 11.5,
-        apr60: 12.8,
-      },
-      "Immediate Repayment": {
-        apr36: 11.25,
-        apr60: 12.55,
-      },
-    },
-    defaultLoanType: "Interest Only", // leave at 0 for interest-only, set to 1 for immediate repayment
-    showMetros: false,
-    showLoanTypes: false,
-    loanTypes: ["Interest Only"],
-    locations: ["Metro A", "Metro B", "Metro C"],
-    metros: [
-      {
-        location: "Metro A",
-        loanInfo: {
-          // match loanInfo to Program 3 loanInfo above
-          maxLoanAmt: 6995,
-          loanTerm36: true,
-          loanTerm60: true,
-          "Interest Only": {
-            k: 6,
-            apr36: 11.5,
-            apr60: 12.8,
-          },
-          "Immediate Repayment": {
-            apr36: 11.25,
-            apr60: 12.55,
-          },
-        },
-      },
-      {
-        location: "Metro B",
-        loanInfo: {
-          maxLoanAmt: 15545,
-          loanTerm36: true,
-          loanTerm60: true,
-          "Interest Only": {
-            k: 6,
-            apr36: 11.59,
-            apr60: 12.84,
-          },
-          "Immediate Repayment": {
-            apr36: 11.25,
-            apr60: 12.55,
-          },
-        },
-      },
-      {
-        location: "Metro C",
-        loanInfo: {
-          maxLoanAmt: 20545,
-          loanTerm36: true,
-          loanTerm60: true,
-          "Interest Only": {
-            k: 6,
-            apr36: 11.59,
-            apr60: 12.84,
-          },
-          "Immediate Repayment": {
-            apr36: 11.25,
-            apr60: 12.55,
-          },
-        },
-      },
-    ],
-  },
+  // {
+  //   name: "Online Part-Time Bootcamp",
+  //   url: "https://my.skills.fund/application?lenderCode=SKDOJON18",
+  //   loanInfo: {
+  //     // match loanInfo in first metro below
+  //     maxLoanAmt: 6995,
+  //     loanTerm36: true,
+  //     loanTerm60: true,
+  //     "Interest Only": {
+  //       k: 6,
+  //       apr36: 11.5,
+  //       apr60: 12.8,
+  //     },
+  //     "Immediate Repayment": {
+  //       apr36: 11.25,
+  //       apr60: 12.55,
+  //     },
+  //   },
+  //   defaultLoanType: "Interest Only", // leave at 0 for interest-only, set to 1 for immediate repayment
+  //   showMetros: false,
+  //   showLoanTypes: false,
+  //   loanTypes: ["Interest Only"],
+  //   locations: ["Metro A", "Metro B", "Metro C"],
+  //   metros: [
+  //     {
+  //       location: "Metro A",
+  //       loanInfo: {
+  //         // match loanInfo to Program 3 loanInfo above
+  //         maxLoanAmt: 6995,
+  //         loanTerm36: true,
+  //         loanTerm60: true,
+  //         "Interest Only": {
+  //           k: 6,
+  //           apr36: 11.5,
+  //           apr60: 12.8,
+  //         },
+  //         "Immediate Repayment": {
+  //           apr36: 11.25,
+  //           apr60: 12.55,
+  //         },
+  //       },
+  //     },
+  //     {
+  //       location: "Metro B",
+  //       loanInfo: {
+  //         maxLoanAmt: 15545,
+  //         loanTerm36: true,
+  //         loanTerm60: true,
+  //         "Interest Only": {
+  //           k: 6,
+  //           apr36: 11.59,
+  //           apr60: 12.84,
+  //         },
+  //         "Immediate Repayment": {
+  //           apr36: 11.25,
+  //           apr60: 12.55,
+  //         },
+  //       },
+  //     },
+  //     {
+  //       location: "Metro C",
+  //       loanInfo: {
+  //         maxLoanAmt: 20545,
+  //         loanTerm36: true,
+  //         loanTerm60: true,
+  //         "Interest Only": {
+  //           k: 6,
+  //           apr36: 11.59,
+  //           apr60: 12.84,
+  //         },
+  //         "Immediate Repayment": {
+  //           apr36: 11.25,
+  //           apr60: 12.55,
+  //         },
+  //       },
+  //     },
+  //   ],
+  // },
   // {
   //   name: "Data Science",
   //   url: "https://my.skills.fund/application?lenderCode=SKCDDS19",
